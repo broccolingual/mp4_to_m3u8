75f218803119074c7c03799d6d3ab6363da041e8
Broccolingual <63525348+broccolingual@users.noreply.github.com>
2021-05-15 12:20:07 +0900

    update: 複数ファイルの探索と自動HLS変換機能の追加

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 import sys, os
 import subprocess
 import codecs
+import glob
 import re
 
 class HLS:
     def __init__(self, path):
-        self.path = path
+        self.path = path.replace(os.sep, '/')
+        self.path_separater = "\\"
+        self.hls_path = self._get_hls_path()
 
     def get_filename(self, filename):
-        m = re.match(r"(?P<filename>[a-zA-Z0-9_]+)\.(?P<extension>[a-zA-Z0-9_]+)", filename)
+        m = re.match(r"(?P<filename>.*)\.(?P<extension>[a-zA-Z0-9_]+)", filename)
         if m:
             return m.group('filename'), m.group('extension')
 
+    def _get_hls_path(self):
+        l = self.path.split("/")
+        filename, _ = self.get_filename(l[-1])
+        l.pop(-1)
+        return f"{'/'.join(l)}/hls/{filename}"
 
     def _create_high_resolution_m3u8(self, segment_time=2):
         l = self.path.split("/")
         filename, _ = self.get_filename(l[-1])
         l.pop(-1)
-        m3u8_path = f"{'/'.join(l)}/{filename}"
-        os.makedirs(f"{m3u8_path}/h", exist_ok=True)
+        os.makedirs(f"{self.hls_path}/h", exist_ok=True)
 
         c = "ffmpeg"
         c += f" -i {self.path}"
         c += " -codec copy -vbsf h264_mp4toannexb -map 0"
         c += f" -f segment -segment_format mpegts -segment_time {segment_time}"
-        c += f" -segment_list {m3u8_path}/h/{filename}_h.m3u8"
-        c += f" {m3u8_path}/h/{filename}_h_%5d.ts"
+        c += f" -segment_list {self.hls_path}/h/{filename}_h.m3u8"
+        c += f" {self.hls_path}/h/{filename}_h_%5d.ts"
 
         code = subprocess.call(c.split())
-        print('process=' + str(code))
+        # print('process=' + str(code))
 
     def _create_low_resolution_mp4(self):
         l = self.path.split("/")
@@ -43,42 +50,40 @@ class HLS:
         c += f' {"/".join(l)}/{filename}_low.{extention}'
 
         code = subprocess.call(c.split())
-        print('process=' + str(code))
+        # print('process=' + str(code))
 
     def _create_low_resolution_m3u8(self, segment_time=2):
         l = self.path.split("/")
         filename, extention = self.get_filename(l[-1])
         l.pop(-1)
-        m3u8_path = f"{'/'.join(l)}/{filename}"
-        os.makedirs(f"{m3u8_path}/l", exist_ok=True)
+        os.makedirs(f"{self.hls_path}/l", exist_ok=True)
 
         c = 'ffmpeg'
         c += f' -i {"/".join(l)}/{filename}_low.{extention}'
         c += ' -codec copy -vbsf h264_mp4toannexb -map 0'
         c += f' -f segment -segment_format mpegts -segment_time {segment_time}'
-        c += f' -segment_list {m3u8_path}/l/{filename}_l.m3u8'
-        c += f' {m3u8_path}/l/{filename}_l_%5d.ts'
+        c += f' -segment_list {self.hls_path}/l/{filename}_l.m3u8'
+        c += f' {self.hls_path}/l/{filename}_l_%5d.ts'
 
         code = subprocess.call(c.split())
-        print('process=' + str(code))
+        # print('process=' + str(code))
 
         os.remove(f'{"/".join(l)}/{filename}_low.{extention}')
 
     def create_hls_high(self):
-        self._create_high_resolution_m3u8()
+        self._create_high_resolution_m3u8(segment_time=5)
 
         l = self.path.split("/")
         filename, _ = self.get_filename(l[-1])
         l.pop(-1)
-        m3u8_path = f"{'/'.join(l)}/{filename}"
-        os.makedirs(m3u8_path, exist_ok=True)
+        os.makedirs(self.hls_path, exist_ok=True)
 
         t = '#EXTM3U'
         t += '\n##EXT-X-VERSION:3'
         t += '\n#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=4000000'
         t += f'\nh/{filename}_h.m3u8'
 
-        f = codecs.open(f'{m3u8_path}/{filename}_playlist.m3u8', 'w', 'utf-8')
+        f = codecs.open(f'{self.hls_path}/{filename}_playlist.m3u8', 'w', 'utf-8')
         f.write(t)
         f.close()
 
@@ -90,8 +95,7 @@ class HLS:
         l = self.path.split("/")
         filename, _ = self.get_filename(l[-1])
         l.pop(-1)
-        m3u8_path = f"{'/'.join(l)}/{filename}"
-        os.makedirs(m3u8_path, exist_ok=True)
+        os.makedirs(self.hls_path, exist_ok=True)
 
         t = '#EXTM3U'
         t += '\n##EXT-X-VERSION:3'
@@ -100,13 +104,23 @@ class HLS:
         t += '\n#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=4000000'
         t += f'\nh/{filename}_h.m3u8'
 
-        f = codecs.open(f'{m3u8_path}/{filename}_playlist.m3u8', 'w', 'utf-8')
+        f = codecs.open(f'{self.hls_path}/{filename}_playlist.m3u8', 'w', 'utf-8')
         f.write(t)
         f.close()
 
 
 if __name__ == "__main__":
     path = sys.argv[1]
+    files = glob.glob(f"{path}/**/*.mp4", recursive=True)
+    print(f"Number of Files: {len(files)}")
 
-    hls = HLS(path)
-    hls.create_hls_high()
+    for file in files:
+        print(f"File Path: {file}")
+        try:
+            hls = HLS(file)
+            hls.create_hls_high()
+        except Exception as e:
+            print(e)
+    
+    # run server(localhost:3000)
+    subprocess.call(f"python -m http.server 3000 --directory {path}/hls")
